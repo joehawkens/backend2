d4ffe106e9beebd52b11beb27f213793e64d83ed
Joe Hawkins <hawkinsjwh@gmail.com>
2023-01-15 13:34:50 -0700

    more fixes...

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,13 +3,32 @@ const router = express.Router()
 
 // Getting all
 router.get('/', (req, res) => {
+    res.send('Hello World.')
 
+})
+
+// Getting one
+router.get('/:id', (req, res) => {
+    
 
 })
 
 
-// Getting one
-router.get('/', (req, res) => {
+// Creating one
+router.post('/', (req, res) => {
+    
+
+})
+
+// Update
+router.patch('/', (req, res) => {
+    
+
+})
+
+
+// Delete
+router.delete('/', (req, res) => {
     
 
 })
